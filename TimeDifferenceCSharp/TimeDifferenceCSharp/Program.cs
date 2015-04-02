5ed7562f022b811b76bc0f3c3b77bf137bbff5c3
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-04-02 23:13:44 +0530

    Rename variable.

--- TimeDifferenceCSharp/TimeDifferenceCSharp/Program.cs
+++ TimeDifferenceCSharp/TimeDifferenceCSharp/Program.cs
@@ -7,9 +7,9 @@ namespace TimeDifferenceCSharp
         static void Main(string[] args)
         {
             DateTime firstDate = DateTime.Parse("12:50");
-            DateTime lastDateTime = DateTime.Parse("10:40");
+            DateTime secondDate = DateTime.Parse("10:40");
 
-            TimeSpan difference = TimeSpan.Parse((firstDate - lastDateTime).ToString());
+            TimeSpan difference = TimeSpan.Parse((firstDate - secondDate).ToString());
 
             Console.Write(difference.Hours);
             Console.WriteLine(difference.Minutes);
