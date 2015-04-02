54ef143d657a309d5c4a89b94c9d2cdaa147871c
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-04-02 23:08:52 +0530

    Initial commit for Time Difference in c#

---
+++ TimeDifferenceCSharp/TimeDifferenceCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TimeDifferenceCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime firstDate = DateTime.Parse("12:50");
+            DateTime lastDateTime = DateTime.Parse("10:40");
+
+            TimeSpan difference = TimeSpan.Parse((firstDate - lastDateTime).ToString());
+
+            Console.Write(difference.Hours);
+            Console.WriteLine(difference.Minutes);
+        }
+    }
+}
